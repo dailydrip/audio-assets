becddafaf5f4bf4a83def9fe7720090fae478af8
Franzé Jr <franzejr@gmail.com>
2017-03-06 16:34:06 -0300

    Solution of episode 013.2. Working with audio assets.

--- App/App.js
+++ App/App.js
@@ -3,23 +3,54 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Platform,
+  Button,
   View
 } from 'react-native';
+import Sound from 'react-native-sound'
+import {AudioRecorder, AudioUtils} from 'react-native-audio'
 
 export default class AudioAssets extends Component {
+
+  constructor(props) {
+    super(props);
+    this.play = this.play.bind(this)
+  }
+
+  async play (fileName) {
+
+    // These timeouts are a hacky workaround for some issues with react-native-sound.
+    // See https://github.com/zmxv/react-native-sound/issues/89.
+    setTimeout(() => {
+      // var sound = new Sound(this.state.audioPath, '', (error) => {
+
+      var initialPath = (Platform.OS === 'ios') ? AudioUtils.MainBundlePath : AudioUtils.DocumentDirectoryPath
+      var sound = new Sound(initialPath + '/' + fileName, '', (error) => {
+        if (error) {
+          console.log('failed to load the sound', error)
+        }
+      })
+
+      setTimeout(() => {
+        sound.play((success) => {
+          if (success) {
+            console.log('successfully finished playing')
+          } else {
+            console.log('playback failed due to audio decoding errors')
+          }
+        })
+      }, 100)
+    }, 100)
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          React Native Assets
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Button title="Play Audio 1" onPress={() => this.play('organfinale.mp3')}/>
+        <Button title="Play Audio 2" onPress={() => this.play('piano.mp3')}/>
       </View>
     );
   }
